aedb0b7a60d6570091175b0c4a7155adab4db0eb
KumailQazi <tag.kumail@gmail.com>
2018-09-19 16:16:56 +0500

    Updates

--- Example.py
+++ Example.py
@@ -12,13 +12,13 @@ c1 = sheet.cell(row = 1, column = 1)
 c1.value = "Raza"
 
 c2 = sheet.cell(row= 1 , column = 2)
-c2.value = "7"
+c2.value = "8"
 
 c3 = sheet['A2']
 c3.value = "Zohair"
 
 # B2 means column = 2 & row = 2.
 c4 = sheet['B2']
-c4.value = "2"
+c4.value = "3"
 
 book.save("Example.xlsx")
